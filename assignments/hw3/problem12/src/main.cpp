438b4cc6c3403d451025a1eaccd7f4a9f4e4ef7d
cakeng <cakeng@snu.ac.kr>
2022-04-18 16:06:07 +0900

    HW3

---
+++ assignments/hw3/problem12/src/main.cpp
@@ -0,0 +1,207 @@
+#define GLM_ENABLE_EXPERIMENTAL
+#define MIN(X,Y) ((X) < (Y) ? (X) : (Y))  
+#define MAX(X,Y) ((X) > (Y) ? (X) : (Y))  
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtx/transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include "shader.h"
+#include "resource_utils.h"
+#include <iostream>
+#include <vector>
+#include "camera.h"
+#include "math_utils.h"
+
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+void processInput(GLFWwindow *window);
+
+bool isKeyboardDone[1024] = {0};
+
+// setting
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+// camera
+Camera camera(glm::vec3(0.0f, 0.0f, 1.0f));
+float lastX = SCR_WIDTH / 2.0f;
+float lastY = SCR_HEIGHT / 2.0f;
+bool firstMouse = true;
+
+
+// timing
+float deltaTime = 0.0f;	// time between current frame and last frame
+float lastFrame = 0.0f;
+
+
+int main()
+{
+    // glfw: initialize and configure
+    // ------------------------------
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // uncomment this statement to fix compilation on OS X
+#endif
+
+    // glfw window creation
+    // --------------------
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwSetCursorPosCallback(window, mouse_callback);
+    glfwSetScrollCallback(window, scroll_callback);
+
+    // tell GLFW to capture our mouse
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+    // glad: load all OpenGL function pointers
+    // ---------------------------------------
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    // configure global opengl state
+    // -----------------------------
+    glEnable(GL_DEPTH_TEST);
+    
+
+    // build and compile our shader program
+    // ------------------------------------
+    // TODO: define 3 shaders
+    // (1) geometry shader for spline render.
+    // (2) simple shader for spline's outer line render.
+    // (optional) (3) tessellation shader for bezier surface.
+
+
+    // TODO : load requied model and save data to VAO. 
+    // Implement and use loadSplineControlPoints/loadBezierSurfaceControlPoints in resource_utils.h
+
+
+
+
+    // render loop
+    // -----------
+    float oldTime = 0;
+    while (!glfwWindowShouldClose(window))
+    {
+        float currentTime = glfwGetTime();
+        float dt = currentTime - oldTime;
+        deltaTime = dt;
+
+        oldTime = currentTime;
+
+        // input
+        // -----
+        processInput(window);
+
+        // render
+        // ------
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+
+        // TODO : render splines
+        // (1) render simple spline with 4 control points for Bezier, Catmull-Rom and B-spline.
+        // (2) render 'u' using Bezier spline
+        // (3) render loop using Catmull-Rom spline and B-spline.
+        // You have to also render outer line of control points!
+
+
+
+
+        // (Optional) TODO : render Bezier surfaces using tessellation shader.
+        
+
+
+
+
+        
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // optional: de-allocate all resources once they've outlived their purpose:
+    // ------------------------------------------------------------------------
+    //glDeleteVertexArrays(1,&VAO);
+    //glDeleteBuffers(1, &VBO);
+
+    // glfw: terminate, clearing all previously allocated GLFW resources.
+    // ------------------------------------------------------------------
+    glfwTerminate();
+    return 0;
+}
+
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+// ---------------------------------------------------------------------------------------------------------
+void processInput(GLFWwindow *window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        camera.ProcessKeyboard(FORWARD, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        camera.ProcessKeyboard(BACKWARD, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        camera.ProcessKeyboard(LEFT, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        camera.ProcessKeyboard(RIGHT, deltaTime);
+
+    // TODO : 
+    // (1) (for spline) if we press key 9, toggle whether to render outer line.
+    // (2) (Optional, for Bezier surface )if we press key 0, toggle GL_FILL and GL_LINE.
+    
+    
+}
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// ---------------------------------------------------------------------------------------------
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    // make sure the viewport matches the new window dimensions; note that width and 
+    // height will be significantly larger than specified on retina displays.
+    glViewport(0, 0, width, height);
+}
+
+
+// glfw: whenever the mouse moves, this callback is called
+// -------------------------------------------------------
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+    if (firstMouse)
+    {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
+    }
+
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+
+    lastX = xpos;
+    lastY = ypos;
+
+    camera.ProcessMouseMovement(xoffset, yoffset);
+}
+
+// glfw: whenever the mouse scroll wheel scrolls, this callback is called
+// ----------------------------------------------------------------------
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+    camera.ProcessMouseScroll(yoffset);
+}
---
+++ assignments/hw3/problem12/src/resource_utils.h
@@ -0,0 +1,24 @@
+#ifndef RESOURCE_UTILS_H
+#define RESOURCE_UTILS_H
+
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <vector>
+#include "opengl_utils.h"
+using namespace std;
+VAO* loadSplineControlPoints(string path)
+{
+	//TODO: load spline control point data and return VAO
+	//You can make use of getVAOFromAttribData in opengl_utils.h
+	
+}
+
+VAO* loadBezierSurfaceControlPoints(string path)
+{
+	//(Optional)TODO: load surface control point data and return VAO.
+	//You can make use of getVAOFromAttribData in opengl_utils.h
+	
+}
+#endif
