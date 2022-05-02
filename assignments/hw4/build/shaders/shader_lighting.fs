7a8378caf702508985f89cfdd393dcdbe697e9d4
cakeng <cakeng@snu.ac.kr>
2022-05-02 17:11:35 +0900

    Prob4 PFM

--- assignments/hw4/build/shaders/shader_lighting.fs
+++ assignments/hw4/build/shaders/shader_lighting.fs
@@ -32,10 +32,35 @@ uniform float useLighting;
 uniform float usePCF;
 uniform float useCSM;
 
+vec2 poissonDisk[16] = vec2[]( 
+   vec2( -0.94201624, -0.39906216 ), 
+   vec2( 0.94558609, -0.76890725 ), 
+   vec2( -0.094184101, -0.92938870 ), 
+   vec2( 0.34495938, 0.29387760 ), 
+   vec2( -0.91588581, 0.45771432 ), 
+   vec2( -0.81544232, -0.87912464 ), 
+   vec2( -0.38277543, 0.27676845 ), 
+   vec2( 0.97484398, 0.75648379 ), 
+   vec2( 0.44323325, -0.97511554 ), 
+   vec2( 0.53742981, -0.47373420 ), 
+   vec2( -0.26496911, -0.41893023 ), 
+   vec2( 0.79197514, 0.19090188 ), 
+   vec2( -0.24188840, 0.99706507 ), 
+   vec2( -0.81409955, 0.91437590 ), 
+   vec2( 0.19984126, 0.78641367 ), 
+   vec2( 0.14383161, -0.14100790 ) 
+);
+
+float random(vec3 seed, int i){
+	vec4 seed4 = vec4(seed,i);
+	float dot_product = dot(seed4, vec4(12.9898,78.233,45.164,94.673));
+	return fract(sin(dot_product) * 43758.5453);
+}
+
 float ShadowCalculation(vec4 fragPosLightSpace)
 {
     float bias = 0.004;  
-    bias = max(0.0005 * (10.0 + 10*dot(Normal, light.dir)), 0.0004);  
+    bias = max(0.0002 * (10.0 + 10*dot(Normal, light.dir)), 0.0004);  
     // perform perspective divide
     vec3 projCoords = fragPosLightSpace.xyz / fragPosLightSpace.w;
     // transform to [0,1] range
@@ -52,15 +77,20 @@ float ShadowCalculation(vec4 fragPosLightSpace)
     {
         shadow = 0.0f;
         vec2 texelSize = 1.0 / textureSize(shadowMap, 0);
-        for(int x = -1; x <= 1; ++x)
+        // for(int x = -1; x <= 1; ++x)
+        // {
+        //     for(int y = -1; y <= 1; ++y)
+        //     {
+        //         float pcfDepth = texture(shadowMap, projCoords.xy + vec2(x, y) * texelSize).r; 
+        //         shadow += currentDepth - bias > pcfDepth ? (1.0/9.0) : 0.0;        
+        //     }    
+        // }
+        for(int i = 0; i < 12; i++)
         {
-            for(int y = -1; y <= 1; ++y)
-            {
-                float pcfDepth = texture(shadowMap, projCoords.xy + vec2(x, y) * texelSize).r; 
-                shadow += currentDepth - bias > pcfDepth ? 1.0 : 0.0;        
-            }    
-        }
-        shadow /= 9.0;
+            int index = int(16.0*random(gl_FragCoord.xyy, i))%16;
+            float pcfDepth = texture(shadowMap, projCoords.xy + poissonDisk[index]/0.2 * texelSize).r; 
+            shadow += currentDepth - bias > pcfDepth ? (1.0/12.0) : 0.0;      
+        } 
     }
 
     return shadow;
